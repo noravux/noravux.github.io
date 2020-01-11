4f28fd020eb2efcd4a190641dad6dd5a80ba7276
Jiri Tukiainen <jiri.tukiainen@edu.savonia.fi>
2020-01-12 00:08:58 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let nav = document.querySelector(".menu");
+let toggler = document.querySelector(".nav-toggler");
+
+toggler.addEventListener("click", function() {
+  nav.classList.toggle("active");
+});
